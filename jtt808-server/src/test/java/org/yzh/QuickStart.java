7ccebb0cccb7980dd9cc54a70f28a1000c681dd1
Jerry Ye <jerry.ye@owon.com>
2025-10-10 11:15:09 +0800

    ~

--- jtt808-server/src/test/java/org/yzh/QuickStart.java
+++ jtt808-server/src/test/java/org/yzh/QuickStart.java
@@ -10,20 +10,25 @@ import org.apache.logging.log4j.Level;
 import org.yzh.commons.util.LogUtils;
 import org.yzh.protocol.codec.JTMessageAdapter;
 import org.yzh.web.endpoint.JTHandlerInterceptor;
+import org.yzh.web.service.DeviceSessionManager;
+import org.yzh.web.service.MessageProducer;
 
 /**
  * 不依赖spring，快速启动netty服务
  */
-public class QuickStart {
+public class QuickStart
+{
     public static final JTMessageAdapter messageAdapter = new JTMessageAdapter("org.yzh.protocol");
     public static final DefaultHandlerMapping handlerMapping = new DefaultHandlerMapping("org.yzh.client");
-    public static final JTHandlerInterceptor handlerInterceptor = new JTHandlerInterceptor(null);
-    public static final SessionManager sessionManager = new SessionManager(new SessionListener() {
+    public static final JTHandlerInterceptor handlerInterceptor = new JTHandlerInterceptor(null, null);
+    public static final SessionManager sessionManager = new SessionManager(new SessionListener()
+    {
     });
 
     public static final int port = 7100;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         LogUtils.setLevel(Level.WARN);
         DispatcherHandler.STOPWATCH = true;
 
