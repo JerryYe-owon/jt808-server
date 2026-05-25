5ed4e475047d4451e33e2650ebce5d5f0f4051b5
Jerry Ye <jerry.ye@owon.com>
2026-05-25 09:31:37 +0800

    Fix tests

--- jtt808-server/src/test/java/org/yzh/web/controller/JT1078ControllerVideo360Test.java
+++ jtt808-server/src/test/java/org/yzh/web/controller/JT1078ControllerVideo360Test.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.yzh.protocol.t1078.T9101;
-import org.yzh.protocol.t1078.T9102;
 import org.yzh.protocol.t808.T0001;
 import org.yzh.web.config.JTProperties;
 import org.yzh.web.endpoint.MessageManager;
@@ -53,7 +52,7 @@ class JT1078ControllerVideo360Test {
         T9101 request = captor.getValue();
         assertSame(response, actual);
         assertEquals(244, request.getChannelNo());
-        assertEquals(1, request.getMediaType());
+        assertEquals(0, request.getMediaType());
         assertEquals("192.168.1.10", request.getIp());
         assertEquals(9100, request.getTcpPort());
         assertEquals(9101, request.getUdpPort());
@@ -78,26 +77,5 @@ class JT1078ControllerVideo360Test {
         verify(messageManager).request(captor.capture(), eq(T0001.class));
         assertEquals(240, captor.getValue().getChannelNo());
         assertEquals(0, captor.getValue().getMediaType());
-    }
-
-    @Test
-    void control360StreamOverridesChannelAndPreservesControlFields() {
-        when(messageManager.request(org.mockito.ArgumentMatchers.any(T9102.class), eq(T0001.class)))
-                .thenReturn(Mono.just(new T0001()));
-        T9102 body = new T9102()
-                .setChannelNo(1)
-                .setCommand(1)
-                .setCloseType(2)
-                .setStreamType(1);
-
-        controller.T9102Video360("keep-rotation", body).block();
-
-        ArgumentCaptor<T9102> captor = ArgumentCaptor.forClass(T9102.class);
-        verify(messageManager).request(captor.capture(), eq(T0001.class));
-        T9102 request = captor.getValue();
-        assertEquals(246, request.getChannelNo());
-        assertEquals(1, request.getCommand());
-        assertEquals(2, request.getCloseType());
-        assertEquals(1, request.getStreamType());
     }
 }
